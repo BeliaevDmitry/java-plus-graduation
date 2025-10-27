9d8efd050ef69e2619afd706026468be21f3d68a
Belyaev Dmitry <_email@gmail.com>
2025-10-27 23:03:25 +0300

    диплом структура application.yaml исправил AdminServiceImpl и ErrorHandler в2(откатил назад)

--- core/main-service/src/main/java/main/service/exception/ErrorHandler.java
+++ core/main-service/src/main/java/main/service/exception/ErrorHandler.java
@@ -90,16 +90,4 @@ public class ErrorHandler {
                 .timestamp(LocalDateTime.now())
                 .build();
     }
-
-    @ExceptionHandler(IllegalStateException.class)
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ApiError illegalStateExceptionHandle(IllegalStateException e) {
-        log.error("Illegal State error: ", e);
-        return ApiError.builder()
-                .status(HttpStatus.CONFLICT)
-                .reason("Некорректное состояние объекта")
-                .message(e.getMessage())
-                .timestamp(LocalDateTime.now())
-                .build();
-    }
 }
